7f4761bdf6a8a75356372150a5f13047663ae093
abdullahN111 <162550451+abdullahN111@users.noreply.github.com>
2024-09-15 00:22:31 +0500

    initialized second next app in repository

---
+++ my-second-next-app/app/contact-us/page.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function Contact() {
+    return ( <div>
+      <nav>
+      <ul className="ul-page flex px-12 pt-6 pb-5 bg-black text-2xl text-white">
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/about">About</Link></li>
+        <li><Link href="/career">Career</Link></li>
+        <li><Link href="/contact-us">Contact Us</Link></li>
+      </ul>
+    </nav>
+      <h1 className="text-3xl pt-5">This is Contact Us Page</h1>
+    
+    </div>
+    );
+  }
+  
---
+++ my-second-next-app/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return ( <div>
+    <nav>
+      <ul className="ul-page flex px-12 pt-6 pb-5 bg-black text-2xl text-white">
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/about">About</Link></li>
+        <li><Link href="/career">Career</Link></li>
+        <li><Link href="/contact-us">Contact Us</Link></li>
+      </ul>
+    </nav>
+    <h1 className="text-3xl pt-5">This is Home Page</h1>
+  </div>
+  );
+}
