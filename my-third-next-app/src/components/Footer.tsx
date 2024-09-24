b6b23dbe49c70b9dad403d309ac841897b1379be
abdullahN111 <162550451+abdullahN111@users.noreply.github.com>
2024-09-24 19:29:24 +0500

    Added Footer Component

---
+++ my-third-next-app/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+
+    return (
+        <>
+        <footer className="h-12 bg-blue-500 text-white fixed bottom-0 w-full"><p className="text-center pt-3"><span>&#169;</span> Copyright Abdullah</p></footer>
+        </>
+    )
+
+}
+
+export default Footer
